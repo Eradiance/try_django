3ac48bc30bba16f4d073717d627cae73f2b7418f
Daniil Radaev <d.radaev99@gmail.com>
2023-02-12 13:11:18 +0300

    les19 Dynamic URL Routing

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import home_view
+from articles import views
 
 urlpatterns = [
     path('', home_view),
+    path('articles/<int:id>/', views.article_home_view),
     path('admin/', admin.site.urls),
 ]
--- trydjango/views.py
+++ trydjango/views.py
@@ -10,9 +10,13 @@ def home_view(request):
     Take in a request
     Return HTML as a response
     """
+
+    queryset_articles = Article.objects.all()
+    print(queryset_articles)
     rundom_id = random.randint(1, 3)
     article_obj = Article.objects.get(id=rundom_id)
     context = {
+        "objects_list": queryset_articles,
         "object": article_obj,
         "title": article_obj.title,
         "id": article_obj.id,
